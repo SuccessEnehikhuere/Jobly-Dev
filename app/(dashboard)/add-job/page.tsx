9446e99d9190a94635d7dfa89c8bb5ed7fe1cbc0
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-11-28 08:46:22 +0100

    "add edit job form, and functionality for editing jobs"

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import CreateJobForm from '@/components/CreateJobForm'
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
 
-function AddJob() {
-  return <div>
-    <CreateJobForm />
-  </div>
+function AddJobPage() {
+  const queryClient = new QueryClient()
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CreateJobForm />
+    </HydrationBoundary>
+  )
 }
-
-export default AddJob
+export default AddJobPage
